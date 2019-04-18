e5688a4400fe861ab8e003c5f30d389d725df6c1
Poojanavgurukul <pooja17@navgurukul.org>
2019-04-18 16:41:05 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,223 @@
+const HomeScreen=[
+    {
+    "model":"Iphone X",
+    "manufacturer":"Apple",
+    "storage":"32 GB",
+    "ios":"IOS",
+    "camera":"40 Mpx",
+    "image":"https://store.stormfront.co.uk/content/images/thumbs/0006976_iphone-x.jpeg",
+    "price":50000
+    },
+    {
+    "model":"Xperia Z1",
+    "manufacturer":"Sony",
+    "storage":"16 GB",
+    "ios":"Andriod",
+    "camera":"13 Mpx",
+    "image":"https://api.sonymobile.com/files/xperia-XZ1-Blue-product-shot-2000x2000-b08e3a68662b814a3ba8f0a7812af0ba.png",
+    "price":10000
+    },
+    {
+    "model":"HTC 856",
+    "manufacturer":"HTC",
+    "storage":"16 GB",
+    "ios":"Andriod",
+    "camera":"13 Mpx",
+    "image":"https://5.imimg.com/data5/TT/CL/MY-49737742/htc-mobile-phones-500x500.jpg",
+    "price":7000
+    },
+    {
+    "model":"Samsung Galaxy A8 Star",
+    "manufacturer":"Samsung",
+    "storage":"16 GB",
+    "ios":"Andriod",
+    "camera":"24 Mpx",
+    "image":"https://www.91-img.com/pictures/129255-v3-samsung-galaxy-a8-star-mobile-phone-large-1.jpg",
+    "price":25994
+    },
+    {
+    "model":"Nokia 3",
+    "manufacturer":"Nokia",
+    "storage":"16 GB",
+    "ios":"Andriod",
+    "camera":"8 Mpx",
+    "image":"https://www.91-img.com/pictures/112575-v2-nokia-3-mobile-phone-large-1.jpg",
+    "price":5999
+    }
+]
+const Apple=[
+    {
+    "model":"Iphone 7",
+    "manufacturer":"Apple",
+    "storage":"32 GB",
+    "ios":"IOS",
+    "camera":"50 Mpx",
+    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRI31MGM0q_XuIeE-390rWSdlHwdSkA1GHhoQSt2-T7enAtwEOx",
+    "price":80000
+    },
+    {
+        "model":"Iphone 8",
+        "manufacturer":"Apple",
+        "storage":"64 GB",
+        "ios":"IOS",
+        "camera":"50 Mpx",
+        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPqy_-VZQndNVjZR9kGzK1yxS2-rcjOhuZ_nrXkHMnh_oP4vkl",
+        "price":70000  
+    },
+    {
+        "model":"Iphone 9",
+        "manufacturer":"Apple",
+        "storage":"128 GB",
+        "ios":"IOS",
+        "camera":"50 Mpx",
+        "image":"https://cdn.iphonelife.com/sites/iphonelife.com/files/styles/screenshot_full_breakpoints_theme_newmango_mobile_2x/public/screen_shot_2018-09-19_at_1.28.39_pm.png?itok=ccTZSkGe",
+        "price":90000   
+    }
+]
+const Sony=[
+    {
+        "model":"SONY XPERIA L3",
+        "manufacturer":"Sony",
+        "storage":"32 GB",
+        "ios":"Andriod",
+        "camera":"13 Mpx",
+        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHXWnJNemSHRJlnAY-yIzJcDE-yYI-SAsGgGhO9Ul2WGxl7AII",
+        "price":14750
+    },
+    {
+        "model":"SONY XPERIA 1",
+        "manufacturer":"Sony",
+        "storage":"64 GB",
+        "ios":"Andriod",
+        "camera":"20 Mpx",
+        "image":"https://images.anandtech.com/doci/14090/IMG_0588_575px.jpg",
+        "price":16000
+    },
+    {
+        "model":"Sony Xperia XA2",
+        "manufacturer":"Sony",
+        "storage":"32 GB",
+        "ios":"Andriod",
+        "camera":"13 Mpx",
+        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPyUzce_YPd4cDfdHILQ3x1pMDB-QA_X3v56VnGChsKtmK2mi6",
+        "price":13000
+    }
+]
+const HTC=[
+    {
+    "model":"HTC Desire 820q",
+    "manufacturer":"HTC",
+    "storage":"16 GB",
+    "ios":"Andriod",
+    "camera":"13 Mpx",
+    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt5Mddz_8xUYbNkm0-gjloWm-yZ9JcnU81txthaIUJ9eDAtwds",
+    "price":11990
+    },
+    {
+    "model":"HTC Desire 10 Pro",
+    "manufacturer":"HTC",
+    "storage":"64 GB",
+    "ios":"Andriod",
+    "camera":"13 Mpx",
+    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqajYZoz7rYmlk5EPAV4GqXYuOAS8uXFmY5MYTrsweQu3LU_tMFg",
+    "price":9999
+    },
+    {
+        "model":"HTC Desire 10 Pro",
+        "manufacturer":"HTC",
+        "storage":"32 GB",
+        "ios":"Andriod",
+        "camera":"13 Mpx",
+        "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTtKO4yapHf5p8w6kJrMIjc8wrDmagTOQBmFGMxpfgWhIckx6ZMA",
+        "price":12000 
+    }
+]
+const Nokia=[
+    {
+    "model":"Nokia 2",
+    "manufacturer":"Nokia",
+    "storage":"8 GB",
+    "ios":"Andriod",
+    "camera":"16 Mpx",
+    "image":"https://images-na.ssl-images-amazon.com/images/I/41HyIaBfrzL.jpg",
+    "price":6250
+    },
+    {
+    "model":"Nokia 6.1",
+    "manufacturer":"Nokia",
+    "storage":"16 GB",
+    "ios":"Andriod",
+    "camera":"16 Mpx",
+    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR197DeRrJYgjBSr1FE8xoqOPWiy36ZKpP-Gw9tiQvLbz93O9qA5w",
+    "price":14800
+    },
+    {
+    "model":"Nokia 7.1",
+    "manufacturer":"Nokia",
+    "storage":"64 GB",
+    "ios":"Andriod",
+    "camera":"12 Mpx",
+    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRs4ay3-WcFiX4_ZZzOG2_Wvsm4FMY_YoHYIfdf1xgbddAHhIZxWg",
+    "price":13000
+    }
+]
+const Samsung=[
+    {
+    "model":"Samsung Galaxy A9",
+    "manufacturer":"Samsung",
+    "storage":"16 GB",
+    "ios":"Andriod",
+    "camera":"24 Mpx",
+    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2DluebB4bWZtsZd45feRR8bFm-yLQ_xgiIFtvGUyNIj8UESGtbQ",
+    "price":27000
+    },
+    {
+    "model":"Samsung Galaxy A7",
+    "manufacturer":"Samsung",
+    "storage":"16 GB",
+    "ios":"Andriod",
+    "camera":"24 Mpx",
+    "image":"https://images.samsung.com/is/image/samsung/in-galaxy-m10-m105f-2gb-sm-m105fdagins-black-thumb-143690854?$PD_SHOP_GALLERY_THUM_PNG$",
+    "price":35000
+    },
+    {
+    "model":"Samsung Galaxy S9",
+    "manufacturer":"Samsung",
+    "storage":"32 GB",
+    "ios":"Andriod",
+    "camera":"48 Mpx",
+    "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1lgMny-Tk4mCyVrNOhjXW2qYt9l0dJYcG04ZP6oxc4ly7wNg2",
+    "price":50000 
+    }
+]
+mydataTemp=(content)=>{
+    return `<div>
+        <img src=${content.image} class="imgaes">
+        <h3>${content.model}</h3>
+        <p>${content.manufacturer}</p>
+        <span>${content.storage}</span>
+        <span>${content.ios}</span>
+        <p>${content.camera}</p>
+        <p>${content.price}</p>
+    </div>`
+}
+onclickApple=()=>{
+    document.getElementById("root").innerHTML=`${Apple.map(mydataTemp).join('')}`
+}
+onclickSamsung=()=>{
+    document.getElementById("root").innerHTML=`${Samsung.map(mydataTemp).join('')}`
+}
+onclickHTC=()=>{
+    document.getElementById("root").innerHTML=`${HTC.map(mydataTemp).join('')}`
+}
+onclickNokia=()=>{
+    document.getElementById("root").innerHTML=`${Nokia.map(mydataTemp).join('')}`
+}
+onclickSony=()=>{
+    document.getElementById("root").innerHTML=`${Sony.map(mydataTemp).join('')}`
+}
+onclickHome=()=>{
+    document.getElementById("root").innerHTML=`${HomeScreen.map(mydataTemp).join('')}`
+}
+document.getElementById("root").innerHTML=`${HomeScreen.map(mydataTemp).join('')}`
+console.log(HomeScreen)
